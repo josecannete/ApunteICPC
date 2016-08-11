d3b77747d65a9234fc262a56d34c5466d3fd1e47
José Cañete <jose.canete@ug.uchile.cl>
2016-08-11 18:34:17 -0400

    Chequeo bipartito funcionando correctamente

--- esBipartito.cpp
+++ esBipartito.cpp
@@ -1,36 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int inf = 1e9;
 
-// Vector que contiene las distancias desde el nodo
-// en que partimos el BFS hasta el resto de los nodos
-vector<int> d;
+// Algoritmo para saber si un grafo es bipartito
+// Se usa para ello la equivalencia bipartito <=> 2-coloreable
+// Lo que hacemos es intentar 2-colorear el grafo mediante un BFS
+
 // Vector que contiene el color de cada nodo
 vector<int> color;
 vector<vector<int> > adj; // Lista de adyacencia
 int n; // Cantidad de nodos
 
-// Algoritmo para saber si un grafo es bipartito
-// Se usa para ello la equivalencia bipartito <=> 2-coloreable
-// Lo que hacemos es intentar 2-colorear el grafo mediante un BFS
-bool esBipartito(int nodo){
-  // Asumimos que el grafo es conexo, en caso de no serlo basta con setear
-  // las distancias como infinito fuera de esta función y luego solo usar la
-  // funcion en cada componente conexa
-  for(int i = 0; i < n; i++)
-    d[i] = inf;
-
-  d[nodo] = 0;
-  color[nodo] = 1;
-
+// Revisamos si una componente conexa es bipartita
+bool esBipartitoConexo(int nodo){
   queue<int> cola;
   cola.push(nodo);
   while( !cola.empty() ){
     int u = cola.front(); cola.pop();
     for (int i = 0; i < adj[u].size(); i++){
       int vecino = adj[u][i];
-      if ( d[vecino] == inf ){
-        d[vecino] = d[u] + 1;
+      if ( color[vecino] == 0 ){
         cola.push(vecino);
         color[vecino] = -1 * color[u];
       }
@@ -44,8 +32,21 @@ bool esBipartito(int nodo){
   return true;
 }
 
+// Función para ver si el grafo entero es bipartito
+bool esBipartito(){
+  bool bipartito = true;
+  for (int i = 1; i <= n; i++){
+    if (color[i] == 0){
+      color[i] = 1;
+      bipartito = bipartito & esBipartitoConexo(i);
+    }
+  }
+  return bipartito;
+}
+
 
 // Ejemplo de uso
+// Ver problema 687A NP-Hard Problem de codeforces
 
 int main(){
 
