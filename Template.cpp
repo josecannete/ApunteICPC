8dc1d94fb40a4003e3862876ec67ef0b08764447
José Cañete <jose.canete@ug.uchile.cl>
2016-08-27 00:50:53 -0300

    Template en construcción

---
+++ Template.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> ii;
+#define pb push_back
+#define ff first
+#define ss second
+
+int main(){
+  std::ios::sync_with_stdio(false);
+
+
+
+  return 0;
+}
