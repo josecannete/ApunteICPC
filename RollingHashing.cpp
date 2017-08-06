288c93cc573bf0d1afb08593ec6ca3db6f6c1b49
José Cañete <jose.canete@ug.uchile.cl>
2017-08-05 23:32:59 -0400

    Rolling Hashing actualizado

--- RollingHashing.cpp
+++ RollingHashing.cpp
@@ -3,34 +3,55 @@ using namespace std;
 
 typedef unsigned long long ull;
 
-int B = 33;
+ull B = 127; // 131
+ull mod = 1e9+7; // 1e9+9
+ull maxstrlen = 5*1e5+5;
+vector<ull> fp;
 
-ull fast_pow(int a,int n) {
-  ull result = 1, power = n, value = a;
-  while (power>0) {
-    if (power&1) result = result * value;
-    value = value * value;
-    power >>= 1;
+void precompPow(ull x){
+  fp.assign(maxstrlen, 0);
+  fp[0] = 1;
+  for (int i = 1; i < maxstrlen; i++){
+    fp[i] = x * fp[i-1]; 
+    fp[i] %= modd;
   }
-  return result;
 }
 
-void construirHash(vector<ull>& h, string s) {
+void construirHash(vector<ull>& h, string& s) {
   h.assign(s.size()+1,0);
-  for (int i = s.size() - 1; i >= 0; i--)
-    h[i] = (ull)s[i] + h[i+1] * B;
+  for (int i = s.size() - 1; i >= 0; i--){
+    h[i] = (ull)s[i] % mod + (h[i+1] * B) % mod;
+    h[i] %= mod;
+  }
 }
 
-ull hashval(vector<ull>& h, int i, int j) {
-  return h[i] - h[j+1] * fast_pow(B, j+1-i);
+ull hashval(vector<ull>& h, ull i, ull j) {
+  return (h[i] % mod - (h[j+1] * fast_pow(B, j+1-i)) % mod + mod) % mod;
 }
+/*
+  Uso: precomputar potencias al menos una vez
+  Se debe entregar indices de la primera letra y ultima del substring
+  que queremos usar
+*/
 
 int main(){
-  string s = "abaaababaaab";
+  precompPow(B);
+  string s = "abcaababcaab";
+  string s2 = "abcaababcaababcaababcaababcaababcaab";
   vector<ull> h;
   construirHash(h, s);
+  vector<ull> h2;
+  construirHash(h2, s2);
   cout << (int)'a' << endl;
   cout << fast_pow(B, 6) << endl;
+  cout << s.substr(0, 11) << endl;
+  cout << hashval(h, 0, 11) << endl;
+  cout << s2.substr(0, 11) << endl;
+  cout << hashval(h2, 0, 11) << endl;
+  cout << hashval(h, 0, 4)  << endl;
+  cout << hashval(h2, 0, 4) << endl;
   cout << hashval(h,0,5) << endl;
   cout << hashval(h,6,11) << endl;
+  cout << hashval(h, 2, 3) << endl;
+  cout << hashval(h, 8, 9) << endl;
 }
