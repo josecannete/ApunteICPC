9851f6d7f60686476d6f95319bab7235e618571f
José Cañete <jose.canete@ug.uchile.cl>
2017-08-11 13:20:47 -0400

    template dp opt

---
+++ templateDivideandConquerDP.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define inf 1e16
+
+typedef long long ll;
+
+vector<vector<ll> > DP;
+
+vector<ll> sumkck, sumck, masder;
+
+ll cost(ll i, ll j){
+}
+
+void solve(ll k, ll l, ll r, ll optl, ll optr){
+  if (l > r) return;
+  ll m = (l+r) / 2;
+  ll optm = l;
+  DP[k][m] = inf;
+  for (ll i = optl; i <= min(r-1, optr); i++){
+    if (DP[k-1][i] + cost(i,m) < DP[k][m]){
+      DP[k][m] = DP[k-1][i] + cost(i,m);
+      optm = i;
+    }
+  }
+
+  solve(k, l, m-1, optl, optm);
+  solve(k, m+1, r, optm, optr);
+}
+
+
+
+
+int main(){
+}
